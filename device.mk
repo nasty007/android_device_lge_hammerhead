2cfc40d4c20c87736a2af7a67082e87531ea3ae8
Prashant Malani <pmalani@google.com>
2016-10-07 18:28:21 -0700

    Add Vibrator default impl to device build
    Bug: 32021191
    Change-Id: Ia838585ca4952d5e706552848254cb05fc969f0e
    Test: m

--- device.mk
+++ device.mk
@@ -425,6 +425,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
