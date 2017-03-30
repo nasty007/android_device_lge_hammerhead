9be493b2de697a17034ce16ecf8f23d64456b91d
Chia-I Wu <olv@google.com>
2017-03-30 11:59:36 -0700

    Enable binderized graphics allocator
    On fugu, SurfaceFlinger must be the graphics allocator.  Set
    SF_START_GRAPHICS_ALLOCATOR_SERVICE for that purpose.
    
    Bug: 36462585
    Test: YouTube, Play Movies
    Change-Id: I3db00645257566a32dc3dd6c2d0e2ca44b16a1dd

--- device.mk
+++ device.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES := \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=19
 
+# Vendor Interface Manifest
+PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/manifest.xml:system/vendor/manifest.xml
+
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
@@ -162,6 +166,10 @@ PRODUCT_PACKAGES += \
     memtrack.msm8974 \
     libqdutils \
     libqdMetaData
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl
 
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
