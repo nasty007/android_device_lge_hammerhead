DirtyUnicorns/android_device_moto_shamu

Add missing liblog dependency
Bug: http://b/31289077
Bug: http://b/27171986
Test: remove liblog from libcutils and recompile
Change-Id: Ib0bb9eff3012f0458bd3bc0d2921da90bab762ca

camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -104,7 +104,7 @@ endif
 LOCAL_CFLAGS += -Wall -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
-         libcutils libdl libmmcamera_interface
+         liblog libcutils libdl libmmcamera_interface
 
 LOCAL_MODULE_TAGS := optional
 
@@ -216,7 +216,7 @@ endif
 LOCAL_CFLAGS += -Wall -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
-         libcutils libdl libmmcamera_interface
+         liblog libcutils libdl libmmcamera_interface
 
 LOCAL_MODULE_TAGS := optional
 

nasty007/android_device_lge_hammerhead

camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -65,7 +65,7 @@ endif
 LOCAL_CFLAGS += -Wall -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
-         libcutils liblog libdl
+         liblog libcutils liblog libdl
 
 LOCAL_MODULE:= mm-qcamera-app
 