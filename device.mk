ed488c9ae6fbdcaf8d5f1ba7456101a9f7924091
Jeff Tinker <jtinker@google.com>
2017-08-27 00:20:43 +0200

    Add drm HALs to device makefiles
    bug:32815560
    Change-Id: I689ad97eade282578e5c3bd9a19bf77e073ae4c1

--- device.mk
+++ device.mk
@@ -452,3 +452,11 @@ $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x74/msm8x74-gpu-vendor.mk)
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
+ifeq ($(ENABLE_TREBLE), true)
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-service
+endif
