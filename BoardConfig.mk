DirtyUnicorns/android_device_moto_shamu

TARGET_FS_CONFIG_GEN: switch from android_filesystem_config.h
Use TARGET_FS_CONFIG_GEN and switch from using android_filesystem_config.h.

BEFORE:
$ md5sum out/target/product/shamu/system/etc/fs_config_files
0be50bbc55a524a0b766c6cf561f8607  out/target/product/shamu/system/etc/fs_config_files

$ ls -laZ out/target/product/shamu/system/etc/fs_config_files
-rw-rw-r-- 1 wcrobert wcrobert ? 88 Mar  4 19:39 out/target/product/shamu/system/etc/fs_config_files

AFTER:

$ md5sum out/target/product/shamu/system/etc/fs_config_files
0be50bbc55a524a0b766c6cf561f8607  out/target/product/shamu/system/etc/fs_config_files

$ ls -laZ out/target/product/shamu/system/etc/fs_config_files
-rw-rw-r-- 1 wcrobert wcrobert ? 88 Mar  4 19:50 out/target/product/shamu/system/etc/fs_config_files

Change-Id: I66cfdf45960eb2e56e1333bdab5677c412c5ded3
Signed-off-by: William Roberts <william.c.roberts@intel.com>

BoardConfig.mk
@@ -136,4 +136,6 @@ EXTENDED_FONT_FOOTPRINT := true
 
 USE_CLANG_PLATFORM_BUILD := true
 
+TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
+
 -include vendor/moto/shamu/BoardConfigVendor.mk

nasty007/android_device_lge_hammerhead

BoardConfig.mk
@@ -123,6 +123,10 @@ ifeq ($(USE_SVELTE_KERNEL),true)
 MALLOC_IMPL := dlmalloc
 endif
 
+USE_CLANG_PLATFORM_BUILD := true
+
+TARGET_FS_CONFIG_GEN += device/lge/hammerhead/config.fs
+
 -include vendor/lge/hammerhead/BoardConfigVendor.mk
 
 # Enable Minikin text layout engine (will be the default soon)